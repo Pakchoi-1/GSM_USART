05d56762a956ef4eadf2b2156ab5f63d4d9469b1
Pakchoi-1 <3118369339@qq.com>
2019-10-10 23:31:47 +0800

    v1.2.0

--- User/main.c
+++ User/main.c
@@ -1,18 +1,22 @@
 	
 #include "include.h"
 
-uint8 sms_data[150]="huangyujieshizhu_GSM TEST";
+uint8 sms_data[100]={0};
+const int Figure[10] = {'0','1','2','3','4','5','6','7','8','9'};
+	u8 temperature;  	    
+	u8 humidity;
 
+uint8  lock=0;
+
+unsigned char send_message(void);
 /**
   * @brief  主函数
   * @param  无  
   * @retval 无
   */
 int main(void)
-{	
-	uint8  lock=0,ret,ok,cot=0;
-	u8 temperature;  	    
-	u8 humidity,t;   
+{	 	    
+	u8 t;   
 	
   delay_init();	    	 //延时函数初始化	 
 	USART_Config();
@@ -40,7 +44,7 @@ int main(void)
 			
 	 	OLED_ShowString(120,0,"*",12);  
 		}				   
-	 	delay_ms(10);
+	 	delay_ms(8);
 		t++;
 		if(t==20)
 		{
@@ -48,42 +52,72 @@ int main(void)
 //   LED1_TOGGLE;
 		GPIOC->ODR&=~(1<<13);
 		}
-	}
-	
-
 		
-	
-	
-//	while (1)
-//	{
-////	gsm_send_cmd("AT\r\n");
-////		delay(2);
-////		gsm_check_network();
-////		delay(2);
-////		gsm_check_CSQ();  
-////		delay(2);
-//		
+		if((lock==0) && strstr(GsmBuf, "+PBREADY"))lock=1;	  //开机消息
+	  if((!(GPIOA->IDR&(1<<1)))&&(lock==2)) lock=1;
+		if(lock==1)	
+		 {	
+			 send_message();
+			 lock=2;
+		 }		
 
-//if((lock==0) && strstr(GsmBuf, "+PBREADY"))ok=1;
-//	else if((lock==0) && (cot==200))
-//		{  ok=1;
-//			cot=0;
-//		}
-//		
-//	delay_ms(10);
-
-//  if(ok==1)
-//	{
-//		gsm_send_message(sms_data);
-//		ok=2;
-//		lock=1;
-//	}
-//		
-//	cot++;	
-//	}
-//	
-
+	}
 }
+
+
+unsigned char send_message(void)
+{
+	char heard[] = "gsm temperature and humidity";
+	
+	int i,j,len;
+	
+	char dat[5];
+	
+	dat[1] = (temperature/10) ;
+	dat[0]= (temperature%10) ;
+	dat[3] = (humidity/10) ;
+	dat[2] =(humidity%10) ;
+	
+	for(j=0;j<4;j++)
+	{
+		for(i=0;i<10;i++)
+		{
+			if(i==dat[j])
+				dat[j]=Figure[i];
+		}
+ }
+	
+	sprintf(sms_data,"\n%s",heard);
+	strcat(sms_data,"\ntemperature:");
+  len = strlen(sms_data);
+  memcpy(sms_data+len, &dat[1], 1);
+  memcpy(sms_data+len+1, &dat[0], 1);
+
+	strcat(sms_data,"\nhumidity:");
+  len = strlen(sms_data);
+  memcpy(sms_data+len, &dat[3], 1);
+  memcpy(sms_data+len+1, &dat[2], 1); 
+	
+	delay_ms(10);
+	
+  gsm_send_cmd("AT\r\n");
+	gsm_send_cmd("AT\r\n");
+	
+  gsm_send_cmd("AT+CSCS=\"GSM\"\r\n"); 			// 设置GSM字符集
+  gsm_send_cmd("AT+CMGF=1\r\n");				// 设置短消息为text模式
+	delay_ms(5);
+	gsm_send_message(sms_data);
+	
+	delay_ms(50);
+	
+	if (strstr(GsmBuf, "OK"))// 命令发送成功
+	{
+	  OLED_ShowString(60,48,"DHT11 scuss",12);
+	
+	}
+	else 	OLED_ShowString(60,48,"DHT11 failed",12);
+}
+
 /*********************************************END OF FILE**********************/
 
 
